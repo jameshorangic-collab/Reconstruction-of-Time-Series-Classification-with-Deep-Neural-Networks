31298dd5e45e6f1d6e968c5c3791313734c77054
jameshorangic-collab <jameshorangic@gmail.com>
2026-08-21 18:40:13 -0400

    Update SGD.py

--- SGD.py
+++ SGD.py
@@ -1,1 +1,6 @@
-
+class SGD:
+    def __init__(self, learningRate):
+        self.learningRate = learningRate
+      
+    def step(self, parameter, gradient):
+        parameter -= self.learningRate * gradient
